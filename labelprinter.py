109005aea8a611b22288425c205c3d5fa0baa2b3
Richard Lin <elpato25@gmail.com>
2018-06-23 11:28:24 -0700

    csv diff detection and printing in labelprinter

--- labelprinter.py
+++ labelprinter.py
@@ -14,14 +14,12 @@ from SvgTemplate import SvgTemplate, TextFilter, ShowFilter, BarcodeFilter, Styl
 from SvgTemplate import strip_tag
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description="Print labels from .csv files")
+  parser = argparse.ArgumentParser(description="Prints labels from changed rows in a .csv file")
 
   parser.add_argument('template', type=str,
                       help="SVG label template")
   parser.add_argument('data', type=str,
                       help="CSV data")
-  parser.add_argument('printed', type=str,
-                      help="CSV of printed columns")
   parser.add_argument('output', type=str,
                       help="SVG generated temporary prefix")
   parser.add_argument('printer', type=str,
@@ -31,7 +29,6 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   ET.register_namespace('', "http://www.w3.org/2000/svg")
-  data_reader = csv.DictReader(codecs.open(args.data, encoding='utf-8'))
 
   if args.only:
     if '=' in args.only:
@@ -60,28 +57,60 @@ if __name__ == '__main__':
   else:
     output_name = args.output
 
+  def canonicalize_row_dict(row_dict):
+    return tuple(sorted([(k, v) for (k, v) in row_dict.items() if v]))
 
-  for (i, row) in enumerate(data_reader):
-    if only_parse_key:
-      if ((only_parse_val is None and not row[only_parse_key]) or
-          (only_parse_val is not None and row[only_parse_key] != only_parse_val)):
-        continue
+  def traverse(file_name, row_dict_fn):
+    data_file = codecs.open(file_name, encoding='utf-8')
+    data_reader = csv.DictReader(data_file)
+    seen_set = set()
+    for row_dict in data_reader:
+      seen_set.add(canonicalize_row_dict(row_dict))
+      row_dict_fn(row_dict)
+    data_file.close()
+    return seen_set
 
-    print(row)
+  def print_fn(seen_set):
+    def actual_fn(row_dict):
+      if (only_parse_key and
+          ((only_parse_val is None and not row[only_parse_key]) or
+           (only_parse_val is not None and row[only_parse_key] != only_parse_val))):
+        return
+      canonical_row_dict = canonicalize_row_dict(row_dict)
+      if canonical_row_dict in seen_set:
+        return
+      print(canonical_row_dict)
 
-    output = template.clone_base()
-    svg_elt = output.getroot()
-    assert strip_tag(svg_elt.tag) == 'svg'
-    for elt in template.generate(row):
-      svg_elt.append(elt)
+      output = template.clone_base()
+      svg_elt = output.getroot()
+      assert strip_tag(svg_elt.tag) == 'svg'
+      for elt in template.generate(row_dict):
+        svg_elt.append(elt)
 
-    output.write(output_name + ".svg")
+      output.write(output_name + ".svg")
 
-    subprocess.Popen([
-      'inkscape', '--without-gui', output_name + '.svg' ,
-      '--export-pdf=' + output_name + '.pdf'
-    ]).communicate()
+      subprocess.Popen([
+        'inkscape', '--without-gui', output_name + '.svg' ,
+        '--export-pdf=' + output_name + '.pdf'
+      ]).communicate()
 
-    win32api.ShellExecute(0,
-      "print", output_name + '.pdf',
-      '/d:"%s"' % args.printer, ".", 0)
+      win32api.ShellExecute(0,
+        "print", output_name + '.pdf',
+        '/d:"%s"' % args.printer, ".", 0)
+    return actual_fn
+
+  # Read in the current rows and ignore those
+  last_mod_time = os.path.getmtime(args.data)
+  seen_set = traverse(args.data, lambda x: x)
+  print("Ready")
+
+  while True:
+    mod_time = os.path.getmtime(args.data)
+
+    if mod_time != last_mod_time:
+      print("File modification detected")
+      seen_set = traverse(args.data, print_fn(seen_set))
+      print("Done")
+
+    last_mod_time = mod_time
+    time.sleep(0.25)
